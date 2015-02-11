c2ff6b7dd252c4f7b506bd999ef61738c65b8a9a
erikcas <erikcas1972@gmail.com>
2015-02-11 11:16:12 +0100

    just build it

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
-
 ifeq ($(TARGET_GPS_HAL_PATH),)
 
 ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
@@ -12,8 +10,6 @@ else
     GPS_DIRS=core utils loc_api platform_lib_abstractions etc
     include $(call all-named-subdir-makefiles,$(GPS_DIRS))
 endif #TARGET_BOARD_PLATFORM
-
-endif
 
 endif
 
